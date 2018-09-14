a93e76d18483fa6e535d5a731c17c5958248de79
Jean Tessier <jean@jeantessier.com>
2018-09-14 10:56:48 -0700

    Upgrade Showdown 0.3.1 --> 1.8.6

--- SoftwareEngineering/AngularShowdownTest/js/myCtrl.js
+++ SoftwareEngineering/AngularShowdownTest/js/myCtrl.js
@@ -3,7 +3,7 @@ angular
     .module("myApp")
 
     .controller("myCtrl", ["$scope", function($scope) {
-        $scope.converter = new Showdown.converter();
+        $scope.converter = new showdown.Converter();
         $scope.textarea = "# Title";
         $scope.markdown = function() {
             return $scope.converter.makeHtml($scope.textarea);
