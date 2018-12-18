2a3099ab98c03f5a034f0c2c868bd6401cf09e16
Jean Tessier <jean@jeantessier.com>
2018-12-17 23:00:10 -0800

    Remove need for extra space at end of line
    The wiki code removes spaces when the next
    line starts an HTML tag.  It was overeager
    and removed the space if the next line
    started with \W, which includes parentheses
    and quotation marks.  I fixed it to look
    for only '<' instead.  Trailing spaces are
    only needed between <a> tags, now.
    
    I also tried to be more consistent with
    putting book titles in italics instead of
    quotation marks.  And to put actual quotes
    in quotation marks instead of italics.
    
    Fixed a few spelling errors.

--- lib/wiki_json.pl
+++ lib/wiki_json.pl
@@ -124,7 +124,7 @@ sub WikiContentsAsJson {
 
     $result =~ s/((\w|")\/?>)\n(<\w)/\1\3/g;
     $result =~ s/\n(<\w)/ \1/g;
-    $result =~ s/\n(\W)/\1/g;
+    $result =~ s/\n(<)/\1/g;
     $result =~ s/([.;])\n/\1  /g;
     $result =~ s/\n/ /g;
 
