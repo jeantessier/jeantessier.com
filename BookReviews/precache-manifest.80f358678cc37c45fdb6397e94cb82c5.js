12357f22da568934ba4bb09dd68431682b10c81e
Jean Tessier <jean@jeantessier.com>
2019-11-29 23:25:50 -0800

    Replace AnguarJS app with React for publishing book reviews

---
+++ BookReviews/precache-manifest.80f358678cc37c45fdb6397e94cb82c5.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bdc9ec245267241a064ad03a064b572",
+    "url": "/BookReviews/index.html"
+  },
+  {
+    "revision": "420e6b07a52323d7013c",
+    "url": "/BookReviews/static/css/main.e253f7ce.chunk.css"
+  },
+  {
+    "revision": "288e73d613cb9b83fe54",
+    "url": "/BookReviews/static/js/2.cac78686.chunk.js"
+  },
+  {
+    "revision": "420e6b07a52323d7013c",
+    "url": "/BookReviews/static/js/main.3f7baf3a.chunk.js"
+  },
+  {
+    "revision": "32eb50f7f490df57ab35",
+    "url": "/BookReviews/static/js/runtime-main.9f869b17.js"
+  },
+  {
+    "revision": "13c073673fed13df2e115720aa3de09a",
+    "url": "/BookReviews/static/media/et-book-bold-line-figures.13c07367.svg"
+  },
+  {
+    "revision": "1c130d09324dbeb3ba2e5a2f9d788a6f",
+    "url": "/BookReviews/static/media/et-book-bold-line-figures.1c130d09.woff"
+  },
+  {
+    "revision": "5294d5d521aab523c8acd8ac96cfe4c0",
+    "url": "/BookReviews/static/media/et-book-bold-line-figures.5294d5d5.ttf"
+  },
+  {
+    "revision": "7ed918f3e796962afd05f4a02fb3f84f",
+    "url": "/BookReviews/static/media/et-book-bold-line-figures.7ed918f3.eot"
+  },
+  {
+    "revision": "0692c0388390239d27fd414060b46df3",
+    "url": "/BookReviews/static/media/et-book-display-italic-old-style-figures.0692c038.svg"
+  },
+  {
+    "revision": "43eb7a3417177f8f0fff766082379da5",
+    "url": "/BookReviews/static/media/et-book-display-italic-old-style-figures.43eb7a34.woff"
+  },
+  {
+    "revision": "a47b89787a4f7491180c40ec69d57998",
+    "url": "/BookReviews/static/media/et-book-display-italic-old-style-figures.a47b8978.eot"
+  },
+  {
+    "revision": "d6175af437d22f14e7cf64c370adc879",
+    "url": "/BookReviews/static/media/et-book-display-italic-old-style-figures.d6175af4.ttf"
+  },
+  {
+    "revision": "10d6df6a575fb60ab98d2a7d3792642a",
+    "url": "/BookReviews/static/media/et-book-roman-line-figures.10d6df6a.woff"
+  },
+  {
+    "revision": "b19f46f303d02748059251cee2aa6abb",
+    "url": "/BookReviews/static/media/et-book-roman-line-figures.b19f46f3.eot"
+  },
+  {
+    "revision": "d54d0bf6aa7fcf3279d8812acb322c06",
+    "url": "/BookReviews/static/media/et-book-roman-line-figures.d54d0bf6.ttf"
+  },
+  {
+    "revision": "f5cdc02ed1cefd2255d254a77c64b7bd",
+    "url": "/BookReviews/static/media/et-book-roman-line-figures.f5cdc02e.svg"
+  },
+  {
+    "revision": "3885093b224ae98aa11a95c8882f1177",
+    "url": "/BookReviews/static/media/et-book-roman-old-style-figures.3885093b.svg"
+  },
+  {
+    "revision": "638c7cda0684c1644411d3849e61995f",
+    "url": "/BookReviews/static/media/et-book-roman-old-style-figures.638c7cda.ttf"
+  },
+  {
+    "revision": "b1b6ddd0b0609c451978017ef81d11b9",
+    "url": "/BookReviews/static/media/et-book-roman-old-style-figures.b1b6ddd0.woff"
+  },
+  {
+    "revision": "fc7b0f1d982d0029eacba5dd89bf8562",
+    "url": "/BookReviews/static/media/et-book-roman-old-style-figures.fc7b0f1d.eot"
+  }
+]);
